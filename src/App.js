c1beaf35765c569bcb6b33b6bd9c8e12bcad8c48
mozainterns <ads.mozasolution@gmail.com>
2019-11-12 16:11:42 +0700

    icon email,

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends Component {
                                         </li>
                                     </ul>
                                 </div>
-
+//
                                 <div className="wrap-icon-header flex-w flex-r-m">
                                     <div
                                         className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
