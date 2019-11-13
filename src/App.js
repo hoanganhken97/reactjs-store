49c4f0fea33dd7fd4478fe520673959356d28280
hoanganhkenmoza <hoanganh@mozasolution.com>
2019-11-13 09:36:35 +0700

    update header

--- src/App.js
+++ src/App.js
@@ -15,106 +15,19 @@ import Contact from "./components/contact/Contact";
 import { connect } from "react-redux";
 import {getCart} from './redux/action/cartAction';
 import About from './components/About';
+import Footer from './components/footer/index';
+import Header from './components/header/index';
 class App extends Component {
     componentDidMount() {
         this.props.getCart();
     }
     render() {
         const {cart} = this.props;
-        console.log('cartRouter: ' + JSON.stringify(cart));
+        // console.log('cartRouter: ' + JSON.stringify(cart));
         return (
 
             <div>
-                <header>
-                    <div className="container-menu-desktop">
-                        <div className="top-bar">
-                            <div className="content-topbar flex-sb-m h-full container">
-                                <div className="left-top-bar">
-                                    Free shipping for standard order over $100
-                                </div>
-
-                                <div className="right-top-bar flex-w h-full">
-                                    <a href="#" className="flex-c-m trans-04 p-lr-25">
-                                        Help & FAQs
-                                    </a>
-
-                                    <a href="#" className="flex-c-m trans-04 p-lr-25">
-                                        My Account
-                                    </a>
-
-                                    <a href="#" className="flex-c-m trans-04 p-lr-25">
-                                        EN
-                                    </a>
-
-                                    <a href="#" className="flex-c-m trans-04 p-lr-25">
-                                        USD
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="wrap-menu-desktop">
-                            <nav className="limiter-menu-desktop container">
-
-                                {/* <a href="#" class="logo">
-						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
-					</a> */}
-
-                                <div className="menu-desktop">
-                                    <ul className="main-menu">
-                                        <li className="active-menu">
-                                            <a href="/">Home</a>
-                                            <ul className="sub-menu">
-                                                <li><a href="/">Homepage 1</a></li>
-                                                <li><a href="/">Homepage 2</a></li>
-                                                <li><a href="/">Homepage 3</a></li>
-                                            </ul>
-                                        </li>
-
-                                        <li>
-                                            <a href="/shop">Shop</a>
-                                        </li>
-
-                                        <li className="label1" data-label1="hot">
-                                            <a href="/features">Features</a>
-                                        </li>
-
-                                        <li>
-                                            <a href="/blog">Blog</a>
-                                        </li>
-
-                                        <li>
-                                            <a href="/about">About</a>
-                                        </li>
-
-                                        <li>
-                                            <a href="/contact">Contact</a>
-                                        </li>
-                                    </ul>
-                                </div>
-//
-                                <div className="wrap-icon-header flex-w flex-r-m">
-                                    <div
-                                        className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-                                        <i className="zmdi zmdi-search"></i>
-                                    </div>
-
-                                    <div
-                                        className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
-                                        data-notify={cart.length}>
-                                        <i className="zmdi zmdi-shopping-cart"></i>
-                                    </div>
-
-                                    <a href="#"
-                                       className="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti"
-                                       data-notify="0">
-                                        <i className="zmdi zmdi-favorite-outline"></i>
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                </header>
+                <Header/>
                 <div className="App-intro">
                     <Switch>
                         <Route exact path="/" component={Home}/>
@@ -130,157 +43,8 @@ class App extends Component {
                     </Switch>
                 </div>
                 {/*<Home/>*/}
-                <footer className="bg3 p-t-75 p-b-32">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-6 col-lg-3 p-b-50">
-                                <h4 className="stext-301 cl0 p-b-30">
-                                    Categories
-                                </h4>
-
-                                <ul>
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Women
-                                        </a>
-                                    </li>
-
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Men
-                                        </a>
-                                    </li>
-
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Shoes
-                                        </a>
-                                    </li>
-
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Watches
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div className="col-sm-6 col-lg-3 p-b-50">
-                                <h4 className="stext-301 cl0 p-b-30">
-                                    Help
-                                </h4>
-
-                                <ul>
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Track Order
-                                        </a>
-                                    </li>
-
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Returns
-                                        </a>
-                                    </li>
-
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            Shipping
-                                        </a>
-                                    </li>
-
-                                    <li className="p-b-10">
-                                        <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                                            FAQs
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div className="col-sm-6 col-lg-3 p-b-50">
-                                <h4 className="stext-301 cl0 p-b-30">
-                                    GET IN TOUCH
-                                </h4>
-
-                                <p className="stext-107 cl7 size-201">
-                                    Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018
-                                    or call us on (+1)
-                                    96 716 6879
-                                </p>
-
-                                <div className="p-t-27">
-                                    <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-                                        <i className="fa fa-facebook"></i>
-                                    </a>
-
-                                    <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-                                        <i className="fa fa-instagram"></i>
-                                    </a>
-
-                                    <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-                                        <i className="fa fa-pinterest-p"></i>
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div className="col-sm-6 col-lg-3 p-b-50">
-                                <h4 className="stext-301 cl0 p-b-30">
-                                    Newsletter
-                                </h4>
-
-                                <form>
-                                    <div className="wrap-input1 w-full p-b-4">
-                                        <input className="input1 bg-none plh1 stext-107 cl7" type="text" name="email"
-                                               placeholder="email@example.com"/>
-                                        <div className="focus-input1 trans-04"></div>
-                                    </div>
-
-
-                                    <div className="p-t-18">
-                                        <button
-                                            className="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
-                                            Subscribe
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
-                        <div className="p-t-40">
-                            <div className="flex-c-m flex-w p-b-18">
-                                <a href="#" className="m-all-1">
-                                    <img src="../../images/icons/icon-pay-01.png" alt="ICON-PAY"/>
-                                </a>
-
-                                <a href="#" className="m-all-1">
-                                    <img src="../../images/icons/icon-pay-02.png" alt="ICON-PAY"/>
-                                </a>
-
-                                <a href="#" className="m-all-1">
-                                    <img src="../../images/icons/icon-pay-03.png" alt="ICON-PAY"/>
-                                </a>
-
-                                <a href="#" className="m-all-1">
-                                    <img src="../../images/icons/icon-pay-04.png" alt="ICON-PAY"/>
-                                </a>
-
-                                <a href="#" className="m-all-1">
-                                    <img src="../../images/icons/icon-pay-05.png" alt="ICON-PAY"/>
-                                </a>
-                            </div>
-
-                            <p className="stext-107 cl6 txt-center">
-
-                                Copyright &copy;
-                                <script>document.write(new Date().getFullYear());</script>
-                                All rights reserved | This template is made with <i className="fa fa-heart-o"
-                                                                                    aria-hidden="true"></i> by <a
-                                href="https://colorlib.com" target="_blank">Colorlib</a>
-
-                            </p>
-                        </div>
-                    </div>
-                </footer>
+                <Footer/>
+                
             </div>
         );
     }
---
+++ src/components/header/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getCart } from "../../redux/action/cartAction";
+import { Link } from 'react-router-dom'
+class Header extends Component {
+  render() {
+    const { cart } = this.props;
+    console.log("cartHeader : " + JSON.stringify(cart));
+    return (
+      <div>
+        <header>
+          <div className="container-menu-desktop">
+            <div className="wrap-menu-desktop">
+              <nav className="limiter-menu-desktop container">
+                <div className="menu-desktop">
+                  <ul className="main-menu">
+                    <li className="active-menu">
+                      <Link to="/">Home</Link>
+                    </li>
+
+                    <li>
+                      <Link to="/shop">Shop</Link>
+                    </li>
+
+                    <li className="label1" data-label1="hot">
+                      <Link to="/features">Features</Link>
+                    </li>
+
+                    <li>
+                      <Link to="/blog">Blog</Link>
+                    </li>
+
+                    <li>
+                      <Link to="/about">About</Link>
+                    </li>
+
+                    <li>
+                      <Link to="/contact">Contact</Link>
+                    </li>
+                  </ul>
+                </div>
+                //
+                <div className="wrap-icon-header flex-w flex-r-m">
+                  <div className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+                    <i className="zmdi zmdi-search"></i>
+                  </div>
+
+                  <div
+                    className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
+                    data-notify={cart.length}
+                  >
+                    <i className="zmdi zmdi-shopping-cart"></i>
+                  </div>
+
+                  <a
+                    href="#"
+                    className="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti"
+                    data-notify="0"
+                  >
+                    <i className="zmdi zmdi-favorite-outline"></i>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return {
+    cart: state.cartReducers
+  };
+}
+
+export default connect(mapStateToProps, { getCart })(Header);
